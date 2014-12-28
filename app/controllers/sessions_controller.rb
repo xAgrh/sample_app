aba17d7e6f5bd4620c3603e64658d81bd31db85d
xAgrh <obsnrd@gmail.com>
2014-12-28 21:38:00 +0400

    Sign in tests. Invalid combinations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Sign the user in and redirect to the user's show page.
+    else
+      # Create an error message and re-render the signin form.
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
 
   end
 
