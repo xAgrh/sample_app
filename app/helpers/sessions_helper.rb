2f9b5e1589f164376624672ba7017f64aed2c822
xAgrh <obsnrd@gmail.com>
2014-12-31 15:45:03 +0400

    User sessions(crypted). Remember token. Signout.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,29 @@
 module SessionsHelper
+
+  def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token # 20.years.from_now
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    self.current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user # Поиск текущего пользователя
+    remember_token = User.encrypt(cookies[:remember_token]) # сначала шифруем
+    @current_user ||= User.find_by(remember_token: remember_token) # потом ищем и присваиваем @current_user если текущщий пользователь не определен
+  end
+
+  def sign_out
+    current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_user = nil
+  end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -23,6 +23,7 @@ describe "Authentication" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
       end
+
     end
 
     describe "with valid information" do
@@ -37,6 +38,11 @@ describe "Authentication" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "folowed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
 
   end
