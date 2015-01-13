41dcbbf7ddb34023d0a2684ce0873a8342c539b1
xAgrh <obsnrd@gmail.com>
2015-01-13 15:26:45 +0400

    Revert "Frendly ation"
    This reverts commit 5ec658355bd5ab4a4d12f169a7ebb0ed384eabc8.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -22,9 +21,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -43,14 +44,6 @@ class UsersController < ApplicationController
     # Before filters
 
     def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
 end
