7f4957762499f70fccd4a95da307b884a05963ce
xAgrh <obsnrd@gmail.com>
2015-01-14 13:14:01 +0400

    User index action + pagination

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem "sass-rails", "~> 4.0.2"
 gem 'bootstrap-sass'
 gem 'bootstrap-sass-extras'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 
 group :development, :test do
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,8 +31,9 @@ describe "Authentication", type: :request do
       before { sign_in user } # method in utilities.rb
 
       it { should have_title(user.name) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
@@ -75,6 +76,11 @@ describe "Authentication", type: :request do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
       end
     end
